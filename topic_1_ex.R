759ebb81d588174d3a20594f8e224400651754c0
ADernild <alex@dernild.dk>
2021-04-07 11:04:30 +0200

    Opsat nogle sektioner og begyndt på 1.4.a

---
+++ topic_1_ex.R
@@ -0,0 +1,42 @@
+# Libraries
+library(st514)
+library(tidyverse)
+library(ggExtra)
+
+
+# Exercise 1.4 ------------------------------------------------------------
+
+data("P1-4") # data
+
+names(tbl) <- c("sales", "profits", "assets") # naming the variables
+
+
+# > a Plot the scatter and marginal dot diagram  --------------------------
+
+scatterplot <- ggplot(tbl, aes(x = sales, y = profits)) +
+  geom_point()
+
+scatterplot
+
+marginal <- ggMarginal(scat, type = "histogram")
+
+marginal
+
+
+# > b Compute sample mean, variance, covariance and correlation co --------
+
+
+
+
+# Exercise 1.5 ------------------------------------------------------------
+
+
+
+# > a Plot scatter and dot diagrams ---------------------------------------
+
+
+
+
+# > b Compute mean, variance-covariance array and correlation array -------
+
+
