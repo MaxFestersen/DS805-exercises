78af71676327d45a01db07116fb7a8508bcfa5aa
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-05-07 15:21:49 +0200

    Added comments.

--- topic_2_ex.R
+++ topic_2_ex.R
@@ -305,13 +305,16 @@ sum(S)
 # > (b) Determine the sample mean and variance of the excess of --------
 # petroleum consumption over natural gas consumption. Also find the sample 
 # covariance of this variable with the total variable in part a.
+# >> Sample mean (of excess)
+mu[1,] - mu[2,]
+
+# >> Sample variance (of excess)
 X.1.S <- S[,1] %>% t() %>% t() # X1 - pretrolium - sample mean
 X.2.S <- S[,2] %>% # X2 - natural gas - sample mean
   matrix(ncol = 1)
 
-mu[1,] - mu[2,]
 
 excess <- X.1.S - X.2.S  # Excess of patrolium consumtion over natural gas sample covariance matrix
-sum(excess)  # Excess of patrolium consumtion over natural gas sample covariance
-
+sum(excess) # Excess of patrolium consumtion over natural gas sample covariance
 excessMean <- mean(excess)
+excessMean # Excess of patrolium consumtion over natural gas sample covariance mean
