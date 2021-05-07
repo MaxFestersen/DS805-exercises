512bd8e352c2a78b06ea6f909e5e48db6195fd1d
ADernild <alex@dernild.dk>
2021-05-07 09:58:23 +0200

    Done

--- topic_2_ex.R
+++ topic_2_ex.R
@@ -50,3 +50,40 @@ B <- matrix(c(1, 2, -2, -1), nrow = 2)
 # Cov(AX^1, BX^2) ---------------------------------------------------------
 
 
+
+# 3.8 Given ---------------------------------------------------------------
+
+S <- matrix(c(1,0,0,0,1,0,0,0,1), nrow = 3)
+
+S_ <- matrix(c(1,-1/2,-1/2,-1/2,1,-1/2,-1/2,-1/2,1), nrow = 3)
+
+
+# > (a) Calculate the total sample variance for each S. Compare the results ------
+
+
+# > (b) Calculate the generalized sample variance for each S, and  --------
+# Comment on the discrepancies, if any, found between Parts a and b.
+
+
+
+# 3.18 Energy consumption in 2001, by state, from the major sources -------
+
+
+# x_1 = petroleum, x_2 = natural gas, x_3 hydroelectric power, x_4 = nuclear electric power
+# is recorded in quadrillions (10^15) of BTUs.
+# The resulting mean and covariance matrix are:
+
+mu <- c(0.766, 0.508, 0.438, 0.161)
+
+S <- matrix(c(0.856, 0.635, 0.173, 0.096, 0.635, 0.568, 0.127, 0.067, 0.173, 0.128, 0.171, 0.039, 0.096, 0.067, 0.039, 0.043), nrow = 4)
+
+
+# > (a) Using the summary statistics, determine the sample mean --------
+# and variance of a state's total energy consumption for these major sources.
+
+
+# > (b) Determine the sample mean and variance of the excess of --------
+# petroleum consumption over natural gas consumption. Also find the sample 
+# covariance of this variable with the total variable in part a.
+
+
